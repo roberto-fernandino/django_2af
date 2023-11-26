5d517f780f6b39e1d1bcd024d07c8cfdc5cea9df
roberto-fernandino <romfernandino@gmail.com>
2023-11-26 11:43:03 -0300

    added some comments to explain admin.

--- app/users/admin.py
+++ app/users/admin.py
@@ -4,47 +4,40 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import UsuarioChangeForm, UsuarioCreationForm
 from .models import Usuario, Codigo
 
+
 # Register your models here.
 class UsuarioAdmin(UserAdmin):
+    # Form no painel do admin para modificar um usuário.
     form = UsuarioChangeForm
+
+    # Form no painel de admin para criar um usuário.
     add_form = UsuarioCreationForm
-    
+
+    # Escolhe quais coisas serão mostradas na pagina admin.
     list_display = [
-        'email',
-        'telefone',
-        'idade',
+        "email",
+        "telefone",
+        "idade",
     ]
-    
+    # Personaliza a página de
     fieldsets = (
-        ("Informações Gerais", {"fields": ['email', 'nome', 'last_login']}),
-        ("informações Pessoas", {'fields': ["telefone", "idade", "uuid", "password"]}),
-        ("Tags", {"fields": ['is_admin', "is_staff", "is_superuser", "is_verified"]}),
+        ("Informações Gerais", {"fields": ["email", "nome", "last_login"]}),
+        ("informações Pessoas", {"fields": ["telefone", "idade", "uuid", "password"]}),
+        ("Tags", {"fields": ["is_admin", "is_staff", "is_superuser", "is_verified"]}),
     )
-    add_fieldsets = [
-        (
-            None,
-            {
-                "classes": ["wide"],
-                "fields": [
-                    "email",
-                    "nome",
-                    "idade",
-                    "telefone",
-                    "password1",
-                    "password2",
-                ],
-            },
-        )
-    ]
-    list_filter = ['is_verified', 'cadastro']
-    search_fields = ['email', 'idade', 'nome']
-    ordering = ['-last_login']
-    filter_horizontal = ['user_permissions']
-    date_hierarchy = 'cadastro'
-    
+    # Permite a filtração por estes dois atributos.
+    list_filter = ["is_verified", "cadastro"]
+
+    # Permite a busca de acordo com estes atributos.
+    search_fields = ["email", "idade", "nome"]
+
+    # Organiza de acordo com um atributo.
+    ordering = ["-last_login"]
+
 
 class CodigoAdmin(admin.ModelAdmin):
-    list_display = ['usuario', 'codigo']
-    
+    list_display = ["usuario", "codigo"]
+
+
 admin.site.register(Codigo, CodigoAdmin)
 admin.site.register(Usuario, UsuarioAdmin)
